ba75eaf6723e3023e6c4a183e220b13433887e03
Rozaliya Altynbaeva <rozalya03@mail.ru>
2026-04-07 17:46:17 +0300

    tsconfig

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+"use strict";
 class RandommerAdapted {
-    private baseUrl = "'https://randommer.io/api/"
-
-    public getRandomName(): Promise<Response> {
+    baseUrl = "'https://randommer.io/api/";
+    getRandomName() {
         let url = `${this.baseUrl}Name?nameType=fullname&quantity=1`;
         return fetch(url, {
             headers: {
                 "X-Api-Key": "568b267ac7eb4b26a718ff57f517fb26"
             }
-        }).then(function(response: Response) {
+        }).then(function (response) {
             return response;
         });
     }
-    
-    public getRandomPhoneNumber(): Promise<Response> {
+    getRandomPhoneNumber() {
         let url = `${this.baseUrl}Phone/Generate?CountryCode=ru&Quantity=1`;
         return fetch(url, {
             headers: {
                 "X-Api-Key": "568b267ac7eb4b26a718ff57f517fb26"
             }
-        }).then(function(response: Response) {
+        }).then(function (response) {
             return response;
         });
     }
 }
-
 let api = new RandommerAdapted();
 let name = api.getRandomName();
 let number = api.getRandomPhoneNumber();
 console.log(name, number);
+//# sourceMappingURL=script.js.map
