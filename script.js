1c05d40adf10a1a166238474601a61e61d96527e
Rozaliya Altynbaeva <rozalya03@mail.ru>
2026-05-04 15:03:46 +0300

    name, number, gender

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ class RandommerAdapted {
             }
         }).then(function (response) {
             return response.json();
+        }).then(function (json) {
+            if (!Array.isArray(json)) {
+                throw new Error("failed to parse /Name response, is not array: " + JSON.stringify(json));
+            }
+            if (json.length == 0) {
+                throw new Error("failed to parse /Name response, array len is 0: " + JSON.stringify(json));
+            }
+            let name = json[0];
+            if (typeof name !== "string") {
+                throw new Error("failed to parse /Name response, first elem is not string: " + JSON.stringify(json));
+            }
+            return name;
         });
     }
     getRandomPhoneNumber() {
@@ -19,12 +31,45 @@ class RandommerAdapted {
             }
         }).then(function (response) {
             return response.json();
+        }).then(function (json) {
+            if (!Array.isArray(json)) {
+                throw new Error("failed to parse /Number response, is not array: " + JSON.stringify(json));
+            }
+            if (json.length == 0) {
+                throw new Error("failed to parse /Number response, array len is 0: " + JSON.stringify(json));
+            }
+            let number = json[0];
+            if (typeof number !== "string") {
+                throw new Error("failed to parse /Number response, first elem is not string: " + JSON.stringify(json));
+            }
+            return number;
         });
+    }
+}
+class GenderizeAdapter {
+    baseUrl = "https://api.genderize.io?";
+    getGender(name) {
+        let url = `${this.baseUrl}name=${name}`;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            const resp = new GenderizeResponseBody(json);
+            return resp.gender;
+        });
+    }
+}
+class GenderizeResponseBody {
+    gender;
+    constructor(obj) {
+        this.gender = obj.gender;
     }
 }
 let api = new RandommerAdapted();
 api.getRandomName().then(function (value) {
-    console.log(value);
+    let a = new GenderizeAdapter();
+    a.getGender(value).then(function (value) {
+        console.log(value);
+    });
 });
 api.getRandomPhoneNumber().then(function (value) {
     console.log(value);
--- script.ts
+++ script.ts
@@ -8,7 +8,19 @@ class RandommerAdapted {
                 "X-Api-Key": "568b267ac7eb4b26a718ff57f517fb26"
             }
         }).then(function(response: Response) {
-            return response.json();
+            return response.json(); 
+        }).then(function(json: any) {
+            if (!Array.isArray(json)) {
+                throw new Error("failed to parse /Name response, is not array: "+JSON.stringify(json))
+            }
+            if (json.length == 0) {
+                throw new Error("failed to parse /Name response, array len is 0: "+JSON.stringify(json))
+            }
+            let name = json[0];
+            if (typeof name !== "string") {
+                throw new Error("failed to parse /Name response, first elem is not string: "+JSON.stringify(json))
+            }
+            return name;
         });
     }
     
@@ -20,13 +32,50 @@ class RandommerAdapted {
             }
         }).then(function(response: Response) {
             return response.json();
+        }).then(function(json: any) {
+            if (!Array.isArray(json)) {
+                throw new Error("failed to parse /Number response, is not array: "+JSON.stringify(json))   
+            }
+            if (json.length == 0) {
+                throw new Error("failed to parse /Number response, array len is 0: "+JSON.stringify(json))
+            }
+            let number = json[0];
+            if (typeof number !== "string") {
+                throw new Error("failed to parse /Number response, first elem is not string: "+JSON.stringify(json))
+            }
+            return number;
+        })
+    }
+}
+
+class GenderizeAdapter {
+    private baseUrl = "https://api.genderize.io?"
+
+    public getGender(name: string): Promise<string> {
+        let url = `${this.baseUrl}name=${name}`
+        return fetch(url).then(function(response: Response) {
+            return response.json();
+        }).then(function(json: any) {
+            const resp = new GenderizeResponseBody(json);
+            return resp.gender
         });
     }
 }
 
+class GenderizeResponseBody {
+    public gender: string;
+    constructor (obj: any) {
+        this.gender = obj.gender
+    }
+}
+
+
 let api = new RandommerAdapted();
 api.getRandomName().then(function(value) {
-    console.log(value);
+    let a = new GenderizeAdapter();
+    a.getGender(value).then(function(value) {
+        console.log(value)
+    })
 })
 api.getRandomPhoneNumber().then(function(value) {
     console.log(value);
